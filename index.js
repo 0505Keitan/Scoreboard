bb509b5d5174f34c002944225c91186d3cf63a80
0505Keitan <0505Keitan@gmail.com>
2019-09-10 23:42:39 +0900

    add : 得点板用ファイルを追加

---
+++ index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+const io = require('socket.io')(http);
+const PORT = process.env.PORT || 3000;
+
+app.get('/' , function(req, res){
+    res.sendFile(__dirname+'/index.html');
+});
+
+app.get('/setting', function(req, res){
+    res.sendFile(__dirname+'/tokuten.html');
+});
+
+
+io.on('connection',function(socket){
+    socket.on('name',function(name){
+        io.emit('name', name);
+    });
+    socket.on('left',function(left){
+        io.emit('left', left);
+    });
+    socket.on('right',function(right){
+        io.emit('right', right);
+    });
+    socket.on('reset', function(reset){
+        io.emit('reset', reset);
+    });
+});
+
+http.listen(PORT, function(){
+    console.log(PORT);
+});
