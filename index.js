51a2643f73e9d52613c83082ee9ac4ab254d1835
0505Keitan <0505Keitan@gmail.com>
2020-01-18 00:50:01 +0900

    update : ci

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/public'));
 
 app.get('/' , function(req, res){
-    res.sendFile(__dirname+'/index.html');
+    es.sendFile(__dirname+'/index.html');
 });
 
 app.get('/view' , function(req, res){
